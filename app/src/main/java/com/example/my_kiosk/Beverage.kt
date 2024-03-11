dbfa2f9129bb7acf482de4d9738e1297a10082fc
Marz2AM <flrudghks78@gmail.com>
2024-03-11 15:15:32 +0900

    키오스크

---
+++ app/src/main/java/com/example/my_kiosk/Beverage.kt
@@ -0,0 +1,4 @@
+package com.example.my_kiosk
+
+class Beverage {
+}
---
+++ app/src/main/java/com/example/my_kiosk/Kiosk.kt
@@ -0,0 +1,11 @@
+package com.example.my_kiosk
+
+fun main() {
+    println("-----메뉴-----")
+    println("덮밥")
+    println("사이드메뉴")
+    println("음료")
+    println("선택해주세요")
+}
+
+open class food() 
